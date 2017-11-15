0930b131c607228bbd06a20d8267259108258d57
ryowo <yoshii@uhura.jp>
2017-11-15 18:14:04 +0900

    Change default .env and app.php

--- config/app.default.php
+++ config/app.default.php
@@ -9,7 +9,7 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
+    'debug' => filter_var(env('DEBUG', env('DEBUG', false)), FILTER_VALIDATE_BOOLEAN),
 
     /**
      * Configure basic information about the application.
@@ -64,7 +64,7 @@ return [
      *   You should treat it as extremely sensitive data.
      */
     'Security' => [
-        'salt' => env('SECURITY_SALT', '__SALT__'),
+        'salt' => env('SECURITY_SALT', env('SECURITY_SALT', null)),
     ],
 
     /**
@@ -222,18 +222,18 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => env('DATABASE_HOST', 'localhost'),
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
-            'encoding' => 'utf8',
-            'timezone' => 'UTC',
+            'username' => env('DATABASE_USERNAME', 'xxxxxx'),
+            'password' => env('DATABASE_PASSWORD', 'xxxxxx'),
+            'database' => env('DATABASE_DATABASE', 'xxxxxx'),
+            'encoding' => env('DATABASE_ENCODING', 'utf8'),
+            'timezone' => env('DATABASE_TIMEZONE', 'Asia/Tokyo'),
             'flags' => [],
             'cacheMetadata' => true,
             'log' => false,
