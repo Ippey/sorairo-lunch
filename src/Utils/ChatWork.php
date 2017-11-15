362b187d42d3e3a3f30b109913ad45686160c600
ryowo <yoshii@uhura.jp>
2017-11-15 22:18:44 +0900

    Add ChatWork Utility (Test method ONLY)

---
+++ src/Utils/ChatWork.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Utils;
+
+/**
+ * ChatWork Utility.
+ */
+class ChatWork
+{
+	/**
+	 * Test Function Add
+	 * 
+	 * @param number $val1
+	 * @param number $val2
+	 * @return number
+	 */
+    public static function add($val1, $val2){
+        return ($val1 + $val2);
+    }
+}
